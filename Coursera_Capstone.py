05d7a9c57bbbbc4a1daf64951f29b3734818a2d9
thesmitpatel <61879629+thesmitpatel@users.noreply.github.com>
2020-07-10 22:15:21 +0530

    Coursera_Capstone

---
+++ Coursera_Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# his notebook will be mainly used for the capstone project.
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
